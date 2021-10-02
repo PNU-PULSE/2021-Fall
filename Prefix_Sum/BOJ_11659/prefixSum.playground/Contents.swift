f0af3732d86f960d9a80af09067be84544036f2a
seonyoung <4tjsdud2@naver.com>
2021-10-02 14:44:08 +0900

    BOJ_11659

---
+++ Prefix_Sum/BOJ_11659/prefixSum.playground/Contents.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+let firstArray = readLine()!.components(separatedBy: " ").map { Int(String($0))!}
+let times = firstArray[1]
+
+let secondArray = readLine()!.components(separatedBy: " ").map { Int(String($0))!}
+
+var sums = [0]
+//sums.reserveCapacity(secondArray.count)
+var sum = 0
+
+secondArray.forEach({
+    sum += $0
+    sums.append(sum)
+})
+
+
+for _ in 0..<times {
+    let sumArray = readLine()!.components(separatedBy: " ").map { Int(String($0))!}
+    let start = sumArray[0]
+    let end = sumArray[1]
+    
+    print(sums[end] - sums[start-1])
+}
