628e689e81f63eb178ea586442bb6e9a9e976f18
ORDINARYCHOI <90915434+ORDINARYCHOI@users.noreply.github.com>
2021-10-10 13:57:51 +0900

    Update BOJ_1992.py

--- Divide_and_Conquer/BOJ_1992/BOJ_1992.py
+++ Divide_and_Conquer/BOJ_1992/BOJ_1992.py
@@ -1,7 +1,7 @@
 def f(x, y, n):
     global ans
     quad = []
-    for i in range(x, x + n):
+    for i in range(x, x + n):  # 해당 사각형의 범위에 해당하는 데이터를 quad에 옮김 
         for j in range(y, y + n):
             k = li[j][i]
             if k == '1':
@@ -12,16 +12,16 @@ def f(x, y, n):
     TF = True
 
     for i in quad:
-        if check != i:
+        if check != i:  # 통일 X인 경우
             ans.append('(')
-            f(x, y, n // 2)
-            f(x + n // 2, y, n // 2)
-            f(x, y + n // 2, n // 2)
-            f(x + n // 2, y + n // 2, n // 2)
+            f(x, y, n // 2)  # 왼쪽 위
+            f(x + n // 2, y, n // 2)  # 오른쪽 위
+            f(x, y + n // 2, n // 2)  # 왼쪽 아래
+            f(x + n // 2, y + n // 2, n // 2)  # 오른쪽 아래
             ans.append(')')
             TF = False
             break
-    if TF:
+    if TF:  # 최종적으로 통일 되어 있는 경우
         ans.append(check)
 
 
